38eb3bc393272ec8192620e60162d3cc24d826de
Charles Holbrow <charlesholbrow@gmail.com>
2018-04-26 14:23:00 -0400

    scrolling behaviro is working well, (but not infinite)

--- src/index.js
+++ src/index.js
@@ -1,44 +1,8 @@
 
 import Tone from 'tone';
 import SVG from 'svg.js';
+import './scroll.js';
 
-var svgRoot = window.svgRoot = document.getElementById('svg-root');
-var outer = window.outer = document.getElementById('outer');
-
-
-var camera = window.camera = {x:0, y: 0};
-var panToCamera = window.panToCamera = function() {
-  var svgBox = svgRoot.getBoundingClientRect();
-  var outerBox = outer.getBoundingClientRect();
-
-  // Panning to scrollXY will put svg origin in the upper left of 'outer'
-  var scrollX = svgBox.width * 0.5;
-  var scrollY = svgBox.height * 0.5;
-
-  // Move the svg origin to the center of the screen
-  scrollX -= (outerBox.width * 0.5);
-  scrollY -= (outerBox.height * 0.5);
-
-  // Then adjust the camera
-  scrollX += camera.x;
-  scrollY += camera.y;
-
-  console.log('Pan to', Math.floor(scrollX), Math.floor(scrollY));
-  outer.scrollTo(Math.floor(scrollX), Math.floor(scrollY));
-};
-
-
-window.addEventListener('resize', function(){
-  console.log('resize:', window.innerWidth, window.innerHeight);
-  panToCamera();
-});
-
-outer.addEventListener('scroll', function(){
-  console.log('scroll', outer.scrollLeft, outer.scrollTop);
-});
-
-
-panToCamera();
 
 var synth = window.synth = new Tone.Sampler({
   61: './sound/delmar-061.wav',
