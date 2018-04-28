09d28a9c11e3091baf71ce7c5e9303adf0165edf
Charles Holbrow <charlesholbrow@gmail.com>
2018-04-28 15:12:53 -0400

    Advance cell rendering strategy

--- src/Cell.js
+++ src/Cell.js
@@ -1,32 +1,76 @@
+import Tone from 'tone';
 import { threeSaturationLevels } from './colors.js';
-
-
 
 export default class Cell {
   constructor(parent, ops){
 
+    // default values
+    ops = typeof ops === 'undefined' ? {} : ops;
     this.hue = typeof ops.hue === 'number' ? ops.hue : Math.random();
     this.size = typeof ops.size === 'number' ? ops.size : 100;
     this.radius = typeof ops.radius === 'number' ? ops.radius : 10;
-
 
     this.colors = threeSaturationLevels(this.hue)
     this.gradient = main.gradient('radial', (stop) => {
       stop.at(0.0, this.colors[2], 1.0);
       stop.at(1.0, this.colors[0], 1.0);
-    }).radius(0.5);
+    }).radius(0.0);
 
     this.svg = parent
       .rect(this.size, this.size)
       .radius(this.radius)
       .fill(this.gradient);
+
+    this.synth = new Tone.Sampler({
+      61: './sound/delmar-end.wav',
+    }, () => {
+
+    }).toMaster();
+
+    this.follower = new Tone.Follower(0.02, 0.5);
+    this.meter = new Tone.Meter(0.9);
+    this.synth.connect(this.meter);
+    this.framesDrawn = 0;
   }
 
   x(value) {
-    return this.svg.x(value);
+    this.svg.x(value);
+    return this;
   }
 
   y(value) {
-    return this.svg.y(value);
+    this.svg.y(value);
+    return this;
+  }
+
+  play() {
+    if (!this.synth.loaded) throw new Error('Synth not loaded');
+    this.framesDrawn = 0;
+    this.synth.triggerAttackRelease(Tone.Frequency(61, 'midi'), 12);
+
+    // This is a little sloppy, because we are playing multiple samples at the
+    // same time, we end up with redundant .draw() calls.
+    this.animate()
+  }
+
+  /**
+   * This should be called once inside each animation frame.
+   * @returns {bool} - finished
+   */
+  draw() {
+    var db = this.meter.getLevel();
+    var gain = db > -240 ?  Tone.dbToGain(db) : 0;
+
+    this.gradient.radius(gain);
+    this.framesDrawn++;
+
+    if (this.framesDrawn > 100 && gain === 0) return true;
+    return false;
+  }
+
+  animate() {
+    window.requestAnimationFrame((hrTime) => {
+      this.draw() || this.animate();
+    });
   }
 }
--- src/index.js
+++ src/index.js
@@ -6,46 +6,26 @@ import Cell from './Cell.js';
 import './scroll.js';
 import { threeSaturationLevels } from './colors.js';
 
+// Never finished
+// Other users can remix
+// Watch other listeners movement
 
-var synth = window.synth = new Tone.Sampler({
-  61: './sound/delmar-061.wav',
-}, () => {
-  console.log('hi222');
-  synth.triggerAttack(Tone.Frequency(50, 'midi'));
-}).toMaster();
+var meter = window.meter = new Tone.Meter(0.1);
+
+window.requestAnimationFrame((hrTimStamp) => {
+
+});
 
 
 var main = window.main = SVG(svgRoot).size(210, 210);
-
-var g1 = main.gradient('radial', (stop) => {
-  stop.at(0.0, '#fff', 1.0);
-  stop.at(1, '#900', 1.0);
-});
-
 
 var size = 100;
 var spacing = 16;
 var cellSize = size + spacing;
 
-var cells = window.cells = [];
+var c1 = new Cell(main).x(0).y(0);
+var c2 = new Cell(main).x(110).y(110);
 
-for (var y = -10; y < 10; y++) {
-  for (var x = -10; x < 10; x++) {
-    if (!Math.floor(Math.random()*8)) {
+var cells = window.cells = [c1, c2];
 
-      var cell = new Cell(main, {size: 100});
-      cell.x(x * size + spacing);
-      cell.y(y * size + spacing);
-      cells.push(cell);
-    }
-  }
-}
-
-var i = 0;
-
-setInterval(() => {
-  for (var i = 0; i < cells.length; i++){
-    var cell = cells[i];
-    cell.gradient.animate(400).radius(2).animate(300).radius(0.5);
-  };
-}, 1500);
+setTimeout(() =>{cells[0].play()}, 1000);
